43e6835f42e7150730ef010697c4942a52247997
Jim Ruther Nill <jim.nill@htw.com.au>
2024-07-14 17:01:44 +1000

    add a bit of styling

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,4 +1,14 @@
 module TasksHelper
+  PRIORITY_MAPPING = {
+    'Low' => 'success',
+    'Medium' => 'warning',
+    'High' => 'danger'
+  }
+
+  def priority_rendering(priority)
+    content_tag(:div, "#{priority} Priority", class: "alert alert-#{PRIORITY_MAPPING[priority]} text-uppercase text-center p-0 py-2 smaller")
+  end
+
   def validation_text(record, attr)
     content_tag(:div, record.errors[attr].to_sentence, class: 'invalid-feedback')
   end
